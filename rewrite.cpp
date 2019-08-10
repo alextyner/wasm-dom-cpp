9b74ed2027284dfb32a1b3a17bb399a2598b56c5
Alex Tyner <36014968+alextyner@users.noreply.github.com>
2019-08-09 21:45:08 -0400

    Rewrite the rewrite because it was becoming a mess. It still could become a mess again.

--- rewrite.cpp
+++ rewrite.cpp
@@ -1,72 +1,78 @@
 #include <iostream>
 #include <memory>
-#include <optional>
+#include <variant>
 #include <string>
 
 class JSObject {
    protected:
    public:
-    JSObject() {}
-
-    template <class T>
-    T cast() {
-        return *reinterpret_cast<T *>(this);
+    template <typename T>
+    T& cast() {
+        return *reinterpret_cast<T*>(this);
     }
     virtual ~JSObject() = default;
 };
 
-class : public JSObject{
-    protected :
-        public :
-
-} window;
-
-class : public JSObject {
-   protected:
-   public:
-    template <class T>
-    static T getElementById(std::string const id) {
-        return T(id);
-    }
-} document;
-
 class HTMLElement : public JSObject {
-   protected:
-    std::string query;
-
-   public:
-    HTMLElement(std::string const query) : query(query) {}
+    private:
+    std::string id;
+    public:
+    HTMLElement() {}
+    HTMLElement(std::string const &id) : id(id) {}
 };
 
 class HTMLCanvasElement : public HTMLElement {
-    class CanvasRenderingContext2D {
-        std::string canvasId;
+   private:
+    class CanvasRenderingContext2D : public HTMLElement {
+       private:
+        CanvasRenderingContext2D() {}
 
        public:
-        CanvasRenderingContext2D(std::string const canvasId) : canvasId(canvasId) {}
-
-        // might not even need canvasid?
-        // what should be stored in query: a query or id or... identifier
+        static CanvasRenderingContext2D const& current() {
+            static CanvasRenderingContext2D current_;
+            return current_;
+        }
     };
-    std::optional<CanvasRenderingContext2D> ctx;
 
    public:
-    HTMLCanvasElement(std::string const query) : HTMLElement(query) {}
-    HTMLCanvasElement(HTMLCanvasElement const &e) : HTMLCanvasElement(e.query) {}
-
-    CanvasRenderingContext2D getContext(std::string const contextType) {
-        if (!ctx)
-            ctx = std::make_optional<CanvasRenderingContext2D>(query);
-        return ctx.value();
-    }
-    std::string getQuery() {
-        return query;
+    CanvasRenderingContext2D const& getContext(std::string const& contextType) {
+        return CanvasRenderingContext2D::current();
     }
 };
 
-int main(int argc, char **argv) {
+class Window : public JSObject {
+   private:
+    Window() {}
+
+   public:
+    static Window const& current() {
+        static Window current_;
+        return current_;
+    }
+};
+
+class HTMLDocument : public JSObject {
+   private:
+    HTMLDocument() {}
+
+   public:
+    static HTMLDocument const& current() {
+        static HTMLDocument current_;
+        return current_;
+    }
+
+    HTMLElement getElementById(std::string const& id) {
+        return HTMLElement(id);
+    }
+};
+
+int main(int argc, char** argv) { /*
     std::cout << "0" << std::endl;
-    JSObject d = document.getElementById<HTMLCanvasElement>("test");
+    JSObject d = document.getElementById("test");
     HTMLCanvasElement c = d.cast<HTMLCanvasElement>();
-    std::cout << c.getQuery() << std::endl;
+    std::cout << c.getQuery() << std::endl;*/
+    auto document = HTMLDocument::current();
+    auto canvas = document.getElementById("id").cast<HTMLCanvasElement>();
+    auto ctx = canvas.getContext("2d");
+
 }
