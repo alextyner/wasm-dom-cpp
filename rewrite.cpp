40037645b3dd39aaa59a4e2c0f4f23f4489611c3
Alex Tyner <36014968+alextyner@users.noreply.github.com>
2019-08-13 00:01:31 -0400

    Progress towards Emscripten integration.

--- rewrite.cpp
+++ rewrite.cpp
@@ -1,78 +1,83 @@
 #include <iostream>
 #include <memory>
-#include <variant>
 #include <string>
+#include <variant>
+#include "../../includes/emscripten/system/include/emscripten.h"
+
+#define STRCAT(x, y) x.y
 
 class JSObject {
-   protected:
-   public:
-    template <typename T>
-    T& cast() {
-        return *reinterpret_cast<T*>(this);
-    }
-    virtual ~JSObject() = default;
+ protected:
+ public:
+  template <typename T>
+  T &cast() {
+    return *reinterpret_cast<T *>(this);
+  }
+  virtual ~JSObject() = default;
 };
 
 class HTMLElement : public JSObject {
-    private:
-    std::string id;
-    public:
-    HTMLElement() {}
-    HTMLElement(std::string const &id) : id(id) {}
+ private:
+  std::string query;
+
+ public:
+  HTMLElement() {}
+  HTMLElement(std::string const &query) : query(query) {}
+  std::string getQuery() { return query; }
 };
 
 class HTMLCanvasElement : public HTMLElement {
+ private:
+  class CanvasRenderingContext2D : public HTMLElement {
    private:
-    class CanvasRenderingContext2D : public HTMLElement {
-       private:
-        CanvasRenderingContext2D() {}
-
-       public:
-        static CanvasRenderingContext2D const& current() {
-            static CanvasRenderingContext2D current_;
-            return current_;
-        }
-    };
+    CanvasRenderingContext2D() {}
 
    public:
-    CanvasRenderingContext2D const& getContext(std::string const& contextType) {
-        return CanvasRenderingContext2D::current();
+    static CanvasRenderingContext2D const &current() {
+      static CanvasRenderingContext2D current_;
+      return current_;
     }
+  };
+
+ public:
+  CanvasRenderingContext2D const &getContext(std::string const &contextType) {
+    return CanvasRenderingContext2D::current();
+  }
 };
 
 class Window : public JSObject {
-   private:
-    Window() {}
+ private:
+  Window() {}
 
-   public:
-    static Window const& current() {
-        static Window current_;
-        return current_;
-    }
+ public:
+  static Window const &current() {
+    static Window current_;
+    return current_;
+  }
 };
 
 class HTMLDocument : public JSObject {
-   private:
-    HTMLDocument() {}
+ private:
+  HTMLDocument() {}
 
-   public:
-    static HTMLDocument const& current() {
-        static HTMLDocument current_;
-        return current_;
-    }
+ public:
+  static HTMLDocument const &current() {
+    static HTMLDocument current_;
+    return current_;
+  }
 
-    HTMLElement getElementById(std::string const& id) {
-        return HTMLElement(id);
-    }
+  static HTMLElement getElementById(std::string const &id) {
+    return HTMLElement(std::string("#").append(id));
+  }
 };
 
-int main(int argc, char** argv) { /*
-    std::cout << "0" << std::endl;
-    JSObject d = document.getElementById("test");
-    HTMLCanvasElement c = d.cast<HTMLCanvasElement>();
-    std::cout << c.getQuery() << std::endl;*/
-    auto document = HTMLDocument::current();
-    auto canvas = document.getElementById("id").cast<HTMLCanvasElement>();
-    auto ctx = canvas.getContext("2d");
-
+int main(int argc, char **argv) {
+  auto document = HTMLDocument::current();
+  auto canvas = document.getElementById("canvas").cast<HTMLCanvasElement>();
+  auto ctx = canvas.getContext("2d");
+  auto window = Window::current();
+  std::cout << canvas.getQuery() << std::endl;
+  EM_ASM({
+    console.log(document.querySelector(UTF8ToString($0)).getContext('2d').strokeStyle);
+  }, canvas.getQuery().c_str());
 }
