23327afe9166eba370c1247119c1b76831baaa28
Alex Tyner <36014968+alextyner@users.noreply.github.com>
2019-08-28 19:45:04 -0400

    Updated readme with info and links to examples.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # wasm-dom-cpp
- 
+
+Inspired by my previous C/DOM interaction project, [wasm-canvas](https://github.com/alextyner/wasm-canvas), and the Java/DOM interface included with the [TeaVM](http://teavm.org/) project, this library aims to make proof-of-concept rendering and simple visualization highly accessible to a lower level language like C++.
+
+This project provides a layer of abstraction for manipulating elements of the HTML DOM -- with a focus on the canvas element in particular -- from C++ compiled with [Emscripten](https://emscripten.org/).
+
+## Goals
+
+Ultimately, I'd like this C++ library to be as feature-complete as the one provided with TeaVM for Java. While possible, such a task would require a more involved approach on the C++ side to make up for the lack of elegant compiler [trickery](http://teavm.org/javadoc/0.5.x/jso/core/org/teavm/jso/JSBody.html) used for the Java implementation.
+
+## Usage
+
+### Headers
+
+This is a header-only library. All headers can be found in the `include/` directory.
+
+You can include `wasm-dom.hpp` for all available functionality,
+
+```c++
+#include "wasm-dom.hpp"
+```
+
+or include individual headers.
+
+```c++
+#include "wasm-dom/HTMLCanvasElement.hpp"
+```
+
+### Compiling
+
+Compile your source code with Emscripten.
+
+```bash
+$ em++ -Wall main.cpp -o index.html
+```
+
+For more involved compilation examples, see the Makefiles for the projects linked below. They can be found within the `test/` directory.
+
+## Examples
+
+[Canvas Rendering Function Tests](https://alextyner.github.io/wasm-dom-cpp/ex-all/)
+
+[Basic Physics Simulation](https://alextyner.github.io/wasm-dom-cpp/ex-physics/) (warning: flashing visuals)
--- test/physics/main.cpp
+++ test/physics/main.cpp
@@ -17,7 +17,6 @@ constexpr double PI = 3.141592653589793238463;
 }
 
 class Vector {
- private:
  public:
   double x, y;
   Vector(double x = 0, double y = 0) : x(x), y(y) {}
@@ -30,7 +29,6 @@ class Ball {
   int radius;
 
  public:
-  Vector v;
   Ball(int x = 0, int y = 0, Vector vel = Vector(), int radius = 10)
       : pos(x, y), vel(vel), radius(radius) {}
   void show() {
@@ -56,31 +54,37 @@ class Ball {
 void update(void *vector) {
   ctx.clearRect(0, 0, width, height);
 
-  for (auto &b : *reinterpret_cast<std::vector<Ball> *>(vector)) {
+  for (auto &b : *static_cast<std::vector<Ball> *>(vector)) {
     b.update();
     b.show();
   }
-  static unsigned short counters[3] = {0x00, 0x00, 0x00};
+  static unsigned char rgbHexParts[3] = {0x00, 0x00, 0x00};
   char style[8];
-  snprintf(style, 8, "#%2.2x%2.2x%2.2x", counters[0], counters[1], counters[2]);
+  sprintf(style, "#%2.2x%2.2x%2.2x", rgbHexParts[0], rgbHexParts[1],
+          rgbHexParts[2]);
   for (int i = 0; i < 3; i++) {
-    counters[i] = rand() % 256;
+    rgbHexParts[i] = rand() % 256;
   }
-  printf("%ld\n", time(NULL));
+  printf("%ld\n", time(NULL));  // roughly track fps
   ctx.setFillStyle(style);
 }
 
-int main(int argc, char **argv) {
+void setUp() {
   canvas.setHeight(height);
   canvas.setWidth(width);
 
   srand(time(NULL));
+}
+
+int main(int argc, char **argv) {
+  setUp();
 
   static std::vector<Ball>
       balls;  // static, otherwise will leave scope after first update() call
   for (int i = 0; i < 20; i++) {
     balls.push_back(Ball(rand() % width, rand() % height,
-                         Vector(rand() % 40 - 20, rand() % 40 - 20), rand() % 35));
+                         Vector(rand() % 40 - 20, rand() % 40 - 20),
+                         rand() % 35));
   }
 
   emscripten_set_main_loop_arg(update, &balls, 0, 0);
